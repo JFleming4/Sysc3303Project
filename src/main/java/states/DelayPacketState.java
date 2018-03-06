91ac649999c93d5d34c959cb593cb722db14452f
DerekStride <djgstride@gmail.com>
2018-03-06 17:47:20 -0500

    Add delay state with tests

---
+++ src/main/java/states/DelayPacketState.java
@@ -0,0 +1,45 @@
+package states;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+
+import socket.TFTPDatagramSocket;
+import util.ErrorChecker;
+
+public class DelayPacketState extends ForwardState {
+	public static final int DEFAULT_DELAY = 1000;
+	
+	private ErrorChecker checker;
+	private long delayInMilliseconds;
+	
+	public DelayPacketState(TFTPDatagramSocket socket, InetAddress serverAddress, ErrorChecker checker, long delayInMilliseconds) {
+		super(socket, serverAddress);
+		this.checker = checker;
+		this.delayInMilliseconds = delayInMilliseconds;
+	}
+	
+	public DelayPacketState(TFTPDatagramSocket socket, InetAddress serverAddress) {
+		this(socket, serverAddress, null, DEFAULT_DELAY);
+	}
+	
+	public void setErrorChecker(ErrorChecker checker) {
+		this.checker = checker;
+	}
+	
+	public void setDelay(long delay) {
+		this.delayInMilliseconds = delay;
+	}
+
+	@Override
+	protected void forwardPacket(DatagramPacket packet) throws IOException {
+		if (checker.check(packet)) {
+			try {
+				Thread.sleep(delayInMilliseconds);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		super.forwardPacket(packet);
+	}
+}
